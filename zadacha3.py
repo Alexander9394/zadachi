5c34de0d474a6b09110a38b90ce76876cb2a768b
Alexander Vingovatov <vingovatov.2022@stud.nstu.ru>
2023-09-25 19:17:02 +0700

    zadacha3

---
+++ zadacha3.py
@@ -0,0 +1,9 @@
+a = int(input('Введите 1-е число: '))
+b = int(input('Введите 2-е число: '))
+
+if a % b == 0:
+        print('Делится')
+        print(a / b)
+else:
+        print('Не делится')
+        print(a / b)
