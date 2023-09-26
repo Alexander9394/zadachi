6bf39e604a3abde5f627e140c3c41dbc52ef2d85
Alexander Vingovatov <vingovatov.2022@stud.nstu.ru>
2023-09-26 18:33:53 +0700

    'zadacha8.1'

--- zadacha8.py
+++ zadacha8.py
@@ -1,9 +1,9 @@
 year = int(input('Введите год: '))
 
-if year % 4 == 0:
-    print('Год високосный.')
+if year % 4 != 0:
+    print('Год не високосный.')
 
-elif year % 100 != 0:
+elif year % 100 == 0:
     if year % 400 == 0:
         print('Год високосный.')
     else:
