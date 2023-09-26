f5ec325a456d53f0b8e955f73e02cf5f7cf01db8
Alexander Vingovatov <vingovatov.2022@stud.nstu.ru>
2023-09-26 18:52:56 +0700

    'zadacha3.1'

--- zadacha3.py
+++ zadacha3.py
@@ -2,8 +2,8 @@ a = int(input('Введите 1-е число: '))
 b = int(input('Введите 2-е число: '))
 
 if a % b == 0:
-        print('Делится')
-        print(a / b)
+        print(a,'Делится',b)
+     
 else:
         print('Не делится')
-        print(a / b)
+        print( "Остаток деления: ", a % b)
